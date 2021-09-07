c50e9a197991c7f6127de02cd200b9258cb85f0f
Vern Chao <vernchao91@gmail.com>
2021-09-07 09:57:44 -0700

    moved around animate and drawPlayerinvoked

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -7,6 +7,7 @@ class Game {
   constructor(ctx, canvas) {
     this.canvas = canvas
     this.ctx = ctx
+    this.fps = 60;
     this.items = new Item(ctx, canvas)
     this.wraith = new Wraith(ctx, canvas)
     this.player = new Player(ctx, canvas)
@@ -14,16 +15,24 @@ class Game {
     this.start();
   }
 
-  draw(ctx) {
-    // ctx.clearRect(0, 0, 400, 400);
-    // ctx.fillStyle = "rgba(0, 0, 0, 0.3)"
-    // ctx.fillRect(0, 0, 400, 400);
-  }
+  // draw(ctx) {
+  //   ctx.clearRect(0, 0, 400, 400);
+  //   ctx.fillStyle = "rgba(0, 0, 0, 0.3)"
+  //   ctx.fillRect(0, 0, 400, 400);
+  // }
 
-  listeners() {
+  eventListeners() {
     window.addEventListener("keydown", this.player.keyDown.bind(this));
     window.addEventListener("keyup", this.player.keyUp.bind(this));
+    // window.addEventListener("keypress", this.startPause.bind(this));
   }
+
+  // startPause(e) {
+  //   if (e.code === "Enter" && this.frames < 2) {
+  //     this.canvas.style.display = "block"
+  //     StartGame.style.display = "none";
+  //   }
+  // }
 
   addMaze() {
   }
@@ -33,10 +42,13 @@ class Game {
 
   start() {
     this.animate();
+    this.eventListeners();
+    requestAnimationFrame(this.animate.bind(this));
   }
 
   animate() {
     this.player.update(this.ctx);
+    requestAnimationFrame(this.animate.bind(this));
   }
 }
 
--- src/scripts/player.js
+++ src/scripts/player.js
@@ -2,36 +2,59 @@ class Player {
   constructor(ctx, canvas) {
     this.canvas = canvas;
     this.ctx = ctx;
-    this.frameX = 0; 
-    this.frameY = 0;
+    this.frameX = 130; 
+    this.frameY = 29;
     this.spriteHeight = 650;
     this.spriteWidth = 650;
-    this.height = this.spriteHeight / 9;
-    this.width = this.spriteWidth / 9;
-    this.x = 130;
-    this.y = 29;
+    this.height = Math.floor(this.spriteHeight / 8.6);
+    this.width = Math.floor(this.spriteWidth / 8.6);
+    this.x = 0;
+    this.y = 0;
+    this.speed = 2;
     this.playerSprite = new Image();
-    this.playerSprite.src = './assets/hero-spritesheet.png'
-    // this.playerSprite.onload = () => this.drawPlayer();
+    this.playerSprite.src = 'src/assets/hero-spritesheet.png'
+    this.playerSprite.onload = () =>this.update();
+    this.keys = [];
+    this.lastInput = "down"
+    // this.ctx.drawImage(this.playerSprite, 130, 29, 650, 650, 0, 0, 75, 75)
   }
   
-  
-  drawPlayer(ctx) {
-    ctx.drawImage(this.playerSprite, 130, 29, 650, 650, 0, 0, 75, 75)
+  update(ctx) {
+    this.drawPlayer();
+    this.animate(ctx);
+    this.move();
+  }
+
+  keyDown(e) {
+    // this.keys[e.keyCode] = true;
+    console.log((e.keyCode + " pressed"));
+    // console.log(this.keys);
+  }
+
+  keyUp(e) {
+    // delete this.keys[e.ekey];
+    console.log((e.keyCode + " released"));
+  }
+
+  // 87w, 65a, 83s, 68d
+  move() {
+    if (this.keys[83] && this.y > 5) {
+      this.y -= this.speed;
+      this.lastInput = "down"
+     }
+  }
+
+  drawPlayer() {
+    this.ctx.drawImage(this.playerSprite, 130, 29, 650, 650, this.x, this.y, 75, 75)
     // ctx.beginPath();
     // ctx.arc(55, 55, 50, 0, 2 * Math.PI);
     // ctx.stroke();
   }
-
-
   
   animate(ctx) {
-    this.drawPlayer(ctx)    
+    
   }
 
-  update(ctx) {
-    this.animate(ctx);
-  }
 
 }
 
