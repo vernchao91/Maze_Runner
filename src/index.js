cfb8b961cfaa5e3d149269e375c6560f281c9bc5
vernchao91 <vernchao91@gmail.com>
2021-12-31 12:04:23 -0800

    removed dark from argument in new Game();

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const mainctx = mainCanvas.getContext("2d");
   const fogctx = fog.getContext("2d");
   // const uictx = dark.getContext("2d");
-  const game = new Game(mainctx, mainCanvas, fog, dark);
+  const game = new Game(mainctx, mainCanvas, fog);
   
   if (!game.gameRunning) {
     game.animateStartMenu();
